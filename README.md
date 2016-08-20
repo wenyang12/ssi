36399910c1ded7db0a17d4d8972585961ff8a8c7
罗瑛 <luoying@fxiaoke.com>
2016-08-20 23:28:46 +0800

    add README.md & require matchs

---
+++ README.md
@@ -0,0 +1,16 @@
+# ssi
+
+> Server Side Include
+> 服务端引入碎片
+
+## file
+相对路径引入。
+```html
+<!--# include file="./test.html"  -->
+```
+
+## virtual
+绝对路径引入。
+```html
+<!--# include virtual="/widgets/test/test.html" -->
+```
--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const getMatchs = require('@tools/matchs');
 const resolvePath = require('@tools/resolve-path');
 
 // 检测页面是否启用ssi
@@ -16,21 +17,14 @@ const REG_INCLUDE = /<!\-\-\#\s*include\s+(file|virtual)=["|'](.*)["|']\s*\-\->/
 // 匹配ssi中的资源引用（排除 a[href="#anchor"]）
 const REG_ASSETS = /(?:src|href|poster)=["|']?([^"'#]+)["|']?/gi;
 
-const getMatchs = (data, reg) => {
-  let matchs = [];
-  let match = null;
-  while ((match = reg.exec(data))) matchs.push(match);
-  return matchs;
-};
-
 // 执行include
-const include = (html, base, root) => {
+const include = (html, dirname, root) => {
   // 扫描html文档，提取出来匹配的include片段
   let matchs = getMatchs(html, REG_INCLUDE);
   matchs.forEach((match) => {
     // 相对路径，相对于请求页面；绝对路径，相对于root
     let isAbsolute = match[1] === 'virtual';
-    let ssiUrl = isAbsolute ? path.join(root, match[2]) : path.resolve(base, match[2]);
+    let ssiUrl = isAbsolute ? path.join(root, match[2]) : path.resolve(dirname, match[2]);
     let ssiHtml = '';
 
     try {
@@ -39,11 +33,11 @@ const include = (html, base, root) => {
       ssiHtml = `<p>ssi error: not found ${match[2]}</p>`;
     }
 
-    let ssiBase = path.dirname(ssiUrl);
+    let ssiDirname = path.dirname(ssiUrl);
     // resolve ssi中引用的静态资源的相对路径
-    ssiHtml = resolvePath(ssiHtml, ssiBase, root, REG_ASSETS);
+    ssiHtml = resolvePath(ssiHtml, ssiDirname, root, REG_ASSETS);
     // 对ssi本身执行include（因为ssi内部也可能有include）
-    ssiHtml = include(ssiHtml, ssiBase, root);
+    ssiHtml = include(ssiHtml, ssiDirname, root);
     // 用include 进来的ssi片段替换include碎片引用
     html = html.replace(match[0], ssiHtml);
   });
